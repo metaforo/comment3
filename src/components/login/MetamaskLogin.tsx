9b557f5374519ddd3699b3594dffa4be85031869
liuwei <linvan6v@gmail.com>
2022-11-18 14:21:49 +0800

    check chain id when user connect by metamask

--- src/components/login/MetamaskLogin.tsx
+++ src/components/login/MetamaskLogin.tsx
@@ -1,14 +1,27 @@
-import {updateUserStatusByLoginResponse, UserInfoState} from "../../context/UserContext";
+import {UserInfoState} from "../../context/UserContext";
 import {typedData} from "../../utils/Util";
 import {loginToEth} from "../../api/ApiService";
-import {LoginType} from "../../utils/Constants";
-import {Storage} from "../../utils/Storage";
 import {Dispatch} from "react";
 import log from "../../utils/LogUtil";
 
 export async function connectToMetamask(setUserState: Dispatch<UserInfoState>) {
     // @ts-ignore
     const {ethereum} = window;
+    const targetNetworkId = '0x1';
+    const chainSwitch = await ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{chainId: targetNetworkId}],
+    }).then((res: any) => {
+        return true;
+    }).catch((e: any) => {
+        return false;
+    });
+    if (!chainSwitch) {
+        return false;
+    } else {
+        await new Promise(resolve => setTimeout(resolve, 500));
+    }
+
     const account = await ethereum.request({
         method: 'eth_requestAccounts'
     }).then((res: any) => {
