94712acf86980dfb64dd5553f443c153f6a4c1b0
liuwei <linvan6v@gmail.com>
2022-11-18 10:34:30 +0800

    fix: can not paste multi-line text in quill editor

--- src/utils/QuillUtil.ts
+++ src/utils/QuillUtil.ts
@@ -3,7 +3,8 @@ import Quill, {StringMap} from "quill";
 import QuillMarkdown from "quilljs-markdown";
 // @ts-ignore
 import QuillMention from "quill-mention";
-import {Blot} from "parchment/src/blot/abstract/blot";
+// @ts-ignore
+import * as Emoji from "quill-emoji";
 
 export const quillModules = (): StringMap => {
     return {
@@ -12,6 +13,7 @@ export const quillModules = (): StringMap => {
         },
         markdownOptions: {},
         mention: {},
+        "emoji-textarea": true,
     };
 }
 
@@ -19,26 +21,5 @@ export function initQuill() {
     // 3rd party
     Quill.register('modules/markdownOptions', QuillMarkdown, true);
     Quill.register('modules/mention', QuillMention, true);
-    Quill.register(emojiBlot(), true);
-}
-
-function emojiBlot(): Blot {
-    const Inline = Quill.import('blots/inline');
-
-    class EmojiBlot extends Inline {
-        static create(value: any) {
-            let node = super.create() as Element;
-            node.innerHTML += '&#x' + value.unicode + ';';
-            return node;
-        }
-
-        length() {
-            return 1;
-        }
-    }
-
-    EmojiBlot.blotName = 'emoji';
-    EmojiBlot.tagName = 'div';
-    // @ts-ignore
-    return EmojiBlot;
+    Quill.register("modules/emoji", Emoji);
 }
