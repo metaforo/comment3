a8606e3105d1bcccdf08ba66af210330af738800
liuwei <linvan6v@gmail.com>
2022-11-28 11:57:38 +0800

    remove bulma css

--- src/components/tipping/EverpayTipDialog.tsx
+++ src/components/tipping/EverpayTipDialog.tsx
@@ -270,6 +270,7 @@ export function EverpayDialog(props: EverpayDialogProps) {
                                     </div>
                                 )}
                                 MenuProps={{
+                                    className: 'mf-main',
                                     PaperProps: {
                                         style: {
                                             maxHeight: 520,
